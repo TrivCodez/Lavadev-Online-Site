141caa1fd2e31b4194e824463c69234eddc9e141
TrivCodez <144015880+TrivCodez@users.noreply.github.com>
2023-10-04 15:12:42 +1000

    Create GradientBackground.js

---
+++ GradientBackground.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const GradientBackground = () => {
+  return (
+    <Background>
+      {/* Content for the landing page */}
+    </Background>
+  );
+};
+
+const Background = styled.div`
+  background: linear-gradient(to right, #ff7f50, #1e90ff);
+  height: 100vh;
+`;
+
+export default GradientBackground;
