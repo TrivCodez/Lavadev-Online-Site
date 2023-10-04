cd6b06b3c17116a4b1ace0ea0252bf2a07ab1e1c
TrivCodez <144015880+TrivCodez@users.noreply.github.com>
2023-10-04 15:14:33 +1000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GradientBackground from "../components/GradientBackground";
+
+const Home = () => {
+  return (
+    <div>
+      <GradientBackground />
+      <h1>Welcome to my landing page!</h1>
+    </div>
+  );
+};
+
+export default Home;
